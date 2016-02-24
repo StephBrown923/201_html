7c26bd4800fb1d0f79142ebb6106ab82137d0c54
StephBrown923 <stephy.mlbrown@gmail.com>
2016-02-24 22:14:15 +0000

    Adding Lab 4

---
+++ nodeThings/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('AJAX.html', { root: 'public' });
+});
+
+/* GET getCity Service */
+router.get('/getcity',function(req,res,next) {
+
+ fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+
+  if(err) throw err;
+   var cities = data.toString().split("\n");
+   var myRe = new RegExp("^" + req.query.q); 
+   var jsonresult = [];
+
+  for(var i = 0; i < cities.length; i++) {
+    var result = cities[i].search(myRe);
+
+     if(result != -1) {
+      jsonresult.push({city:cities[i]});
+     }
+
+   }
+   res.status(200).json(jsonresult);
+ })
+
+});
+
+
+module.exports = router;
